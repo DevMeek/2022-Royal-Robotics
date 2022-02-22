4cd1cd37abc99bcda3bf709c68a1057f625d8285
DevMeek <devonmeekrrhs@gmail.com>
2022-02-22 14:54:53 -0500

    small changes

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,13 +3,18 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
-
+#include "controllerclass.h"
+#include "DriveTrain.h"
+#include "limelight.h"
 DriveTrainClass * drivetrain;
 controllerclass * controller1;
+class limelight * limelight;
 void Robot::RobotInit() {
+
   drivetrain = new DriveTrainClass();
   controller1 = new controllerclass();
   drivetrain->initDriveTrainClass();
+  limelight = new class limelight;
 }
 void Robot::RobotPeriodic() {}
 
@@ -18,7 +23,11 @@ void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {
-  drivetrain->TankDrive(controller1->dleftstickYC1, controller1->drightstickYC1);
+  
+  limelight->getHorizontal(controller1->bAbuttonpressedC1);
+  drivetrain->TankDrive(controller1->dleftstickYC1 + limelight->steeringAdjust, controller1->drightstickYC1 - limelight->steeringAdjust);
+  // drivetrain->TankDrive(controller1->dleftstickYC1,controller1->drightstickYC1);
+  
 }
 
 void Robot::DisabledInit() {}
